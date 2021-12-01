18204f8d25aafe86cce2905466f59fc5c6b2126a
zhxiaokang <zhxiaokang@gmail.com>
2021-12-01 23:40:53 +0100

    correlation between E2F7 and chemokine

---
+++ scripts/explore_expression.R
@@ -0,0 +1,42 @@
+# Exploration of gene expression, like correlation between interesting genes
+
+# Set dir
+setwd("/Users/xiaokangzhang/github/ESCC_TIME/scripts/")
+
+# load libs
+library(dplyr)
+library(ggplot2)
+library(corrr)
+
+# Load data
+load("/Users/xiaokangzhang/github/ESCC_TIME/data/esca_tcga/escc_tcga.RData")
+dir.out <- "/Users/xiaokangzhang/github/ESCC_TIME/output/"
+
+# # ====== gene expression of E2F family genes =======
+# df.exp.e2f <- select(df.exp.inter, starts_with("E2F"))
+# 
+# ave.exp.e2f <- apply(df.exp.e2f, 2, function(x) {log2(sum(x)/length(x))})
+# df.ave.exp.e2f <- data.frame(gene = names(ave.exp.e2f), exp = c(ave.exp.e2f))
+# 
+# ggplot(data = df.ave.exp.e2f, aes(x = gene, y = exp)) +
+#   geom_bar(stat = "identity") +
+#   scale_x_discrete("Genes") +
+#   scale_y_discrete("Expression (log2)")
+
+# ======= correlation between E2F7 and chemokine genes ============
+df.chemokine <- select(df.exp.inter, starts_with("CXCL") | starts_with("CCL"))
+exp.e2f7 <- select(df.exp.inter, c(E2F7))
+
+corr.e2f7.with.chemokine <- cor(exp.e2f7, df.chemokine, method = "pearson")
+
+df.corr.e2f7.with.chemokine <- data.frame(chem = colnames(corr.e2f7.with.chemokine), pearson = corr.e2f7.with.chemokine[1, ])
+
+pdf(file = file.path(dir.out, "corr_chemokine_e2f7.pdf"))
+
+ggplot(data = df.corr.e2f7.with.chemokine, aes(x = chem, y = pearson)) +
+  geom_bar(stat = "identity") +
+  labs(x = "Chemokine genes", y = "Pearson correlaton", title = "Correlation between E2F7 and chemokine genes") +
+  theme(plot.title = element_text(hjust = 0.5)) +
+  coord_flip()
+
+dev.off()
